78cb8a37f04e2363e87fd1b0fc0198befc91b864
sarahayu <46186848+sarahayu@users.noreply.github.com>
2021-01-24 01:19:06 -0800

    implemented proper timekeeping and framechange indexing

--- badapple.cpp
+++ badapple.cpp
@@ -14,70 +14,65 @@ struct Change
 	char ch;
 };
 
+typedef long long llong;
+
 int main()
 {
-	const int FRAME_DURATION = 1000 / 30;
-/*
-	std::vector<std::vector<Change>> changes;
-	std::ifstream changeFile("changes.txt");
-
-	int amtChanges;
-	while (changeFile >> amtChanges)
-	{
-		changes.push_back(std::vector<Change>(amtChanges));
-		auto &curFrame = changes.back();
-		for (int i = 0; i < amtChanges; i++)
-		{
-			Change change;
-			changeFile >> change.x >> change.y >> change.ch;
-			if (change.ch == 'd') change.ch = ' ';
-			curFrame[i] = change;
-		}
-	}
-*/
+	const llong FRAME_DURATION = 1000000 / 30;	// in microseconds
 
 	HANDLE hOut;
-	COORD Position;
-
 	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-
-
 	std::ios_base::sync_with_stdio(false);
-
-
-	//int frame = 0;
-	//const int len = changes.size();
-	//while (frame < len)
-	//{
-	//	for (const auto &frame : changes[frame])
-	//	{
-	//		Position.X = frame.x;
-	//		Position.Y = frame.y;
-	//		SetConsoleCursorPosition(hOut, Position);
-	//		std::cout << frame.ch;
-	//	}
-
-	//	frame++;
-	//	//std::cout << frames::list[0];
-	//	std::this_thread::sleep_for(std::chrono::milliseconds(FRAME_DURATION));
-	//}
 
 
 	int frame = 0;
 	const int len = a.size();
+	const int width = 22, height = 10;
 
-	using ch = std::chrono::steady_clock;
+	namespace sch = std::chrono;
+	using sc = sch::steady_clock;
 
-	auto before = ch::now();
+	auto before = sc::now();
+	std::string lastFrame = a[0];
 	while (frame < len)
 	{
-		Position.X = 0;
-		Position.Y = 0;
-		SetConsoleCursorPosition(hOut, Position);
-		std::cout << a[frame];
-		frame++;
-		std::this_thread::sleep_for(std::chrono::milliseconds(FRAME_DURATION));
+		auto after = sc::now();
+		auto dt = sch::duration_cast<sch::microseconds>(after - before).count();
+
+		bool drawNewFrame = false;
+		if (drawNewFrame = (dt >= FRAME_DURATION))
+			before = after;
+
+		std::string curFrame = lastFrame;
+		while (dt >= FRAME_DURATION)
+		{
+			dt -= FRAME_DURATION;
+
+			SetConsoleCursorPosition(hOut, { 0, 0 });
+
+			curFrame = a[frame];
+			frame++;
+
+			if (frame >= len) break;
+		}
+
+		if (drawNewFrame)
+		{
+			std::vector<Change> changes;
+			for (int i = 0; i < curFrame.length(); i++)
+				if (curFrame[i] != lastFrame[i])
+					changes.push_back({ (short)(i % (width + 1)), (short)(i / (width + 1)), curFrame[i] });
+
+			for (const auto &change : changes)
+			{
+				SetConsoleCursorPosition(hOut, { change.x, change.y });
+				std::cout << change.ch;
+			}
+
+			lastFrame = curFrame;
+		}
 	}
+
 	std::cin.get();
 
 	return 0;
